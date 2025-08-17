106220d88b41af7ab492859a52081895733e88cc
xxx <hgfh@dfds.ddd>
2025-08-17 10:23:30 +0300

    fix

--- app/pages/contacts.vue
+++ app/pages/contacts.vue
@@ -21,55 +21,12 @@ useSeoMeta({
   <section>
     <div class="container">
       <img class="rounded-2xl h-[300px] object-cover w-full mb-10"  src="~/assets/images/other.png" alt="">
-
-
       <h1 class="font-medium text-2xl md:text-4xl mb-5">{{t('breadcrumb_contacts')}}</h1>
       <p class="opacity-60  whitespace-pre-line mb-10">{{t('contacts_text')}}</p>
-
-
     </div>
   </section>
 
-  <section>
-    <div class="container">
-      <p class="text-sm  opacity-60 mb-4 md:mb-9">{{t('contacts_tag_all')}}</p>
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-3">
-        <div class="bg-gray-50 p-7 rounded-xl ">
-          <p class="text-2xl font-medium mb-9">{{t('contacts_phone')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap">{{t('contacts_ru_call')}}</p>
-          <a class="opacity-90 text-lg font-medium block " href="tel:+79633785752">+7 963 378 57 52</a>
-
-
-        </div>
-        <div class="bg-gray-50 p-7  rounded-xl">
-          <p class="text-2xl font-medium mb-9">{{t('contacts_email')}}</p>
-          <a class="opacity-60 text-lg font-medium " href="mailto:uet2022@mail.ru">uet2022@mail.ru</a>
-        </div>
-        <div class="bg-gray-50 p-7  rounded-xl">
-          <p class="text-2xl font-medium mb-9">{{t('contacts_time')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap">{{t('contacts_time_msk')}}</p>
-          <p class="opacity-90 text-lg font-medium block border-b mb-4 pb-4" >{{t('contacts_time_msk_val')}}</p>
-
-          <p class="opacity-60 text-sm whitespace-pre-wrap">{{t('contacts_time_days')}}</p>
-          <p class="opacity-90 text-lg font-medium " >{{t('contacts_time_days_val')}}</p>
-
-        </div>
-        <div class="bg-gray-50 p-7  rounded-xl">
-          <p class="text-2xl font-medium mb-9">РЕКВИЗИТЫ</p>
-          <p class=" whitespace-pre-wrap">ООО "Юнайтед Экспо Трейд"</p>
-
-          <p class="opacity-60 text-sm whitespace-pre-wrap">ИНН / КПП</p>
-          <p class="opacity-90 text-lg font-medium block" >7714486727 / 770801001</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap">ОГРН</p>
-          <p class="opacity-90 text-lg font-medium block" >1227700337204</p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-
-
-  <section>
+  <section class="mb-10">
     <div class="container">
       <p class="text-sm  opacity-60 mb-4 md:mb-9">{{t('contacts_tag_ru')}}</p>
       <div class="grid grid-cols-4 gap-3">
@@ -82,30 +39,34 @@ useSeoMeta({
         </div>
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_store_address')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ru_address3')}}</p>
-  </div>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title1')}}</p>
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_ru_new_text_1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title2')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ru_new_text_2')}}</p>
+        </div>
         <div class="col-span-2 rounded-xl">
           <img class="rounded-xl" src="~assets/images/ru.png" alt="">
         </div>
       </div>
     </div>
   </section>
-
-  <section>
+  <section class="mb-10">
     <div class="container">
       <p class="text-sm  opacity-60 mb-4 md:mb-9">{{t('contacts_tag_ar')}}</p>
       <div class="grid grid-cols-4 gap-3">
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_address')}}</p>
           <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ar_address1')}}</p>
-
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_ar_address1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_fact_address')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ar_address2')}}</p>
         </div>
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_store_address')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ar_address2')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title1')}}</p>
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_ar_new_text_1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title2')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_ar_new_text_2')}}</p>
         </div>
         <div class="col-span-2 rounded-xl">
           <img class="rounded-xl" src="~assets/images/ar.png" alt="">
@@ -114,20 +75,23 @@ useSeoMeta({
     </div>
   </section>
 
-  <section>
+  <section class="mb-10">
     <div class="container">
       <p class="text-sm  opacity-60 mb-4 md:mb-9">{{t('contacts_tag_gr')}}</p>
       <div class="grid grid-cols-4 gap-3">
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_address')}}</p>
           <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_gr_address1')}}</p>
-
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_gr_address1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_fact_address')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_gr_address2')}}</p>
         </div>
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_store_address')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_gr_address2')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title1')}}</p>
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_gr_new_text_1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title2')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_gr_new_text_2')}}</p>
         </div>
         <div class="col-span-2 rounded-xl">
           <img class="rounded-xl" src="~assets/images/gr.png" alt="">
@@ -142,13 +106,17 @@ useSeoMeta({
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_address')}}</p>
           <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_tr_address1')}}</p>
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_tr_address1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_fact_address')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_tr_address2')}}</p>
 
         </div>
         <div class="col-span-1 bg-gray-50 p-7">
           <p class="text-2xl font-medium mb-9">{{t('contacts_card_store_address')}}</p>
-          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_ur_address')}}</p>
-          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_tr_address2')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title1')}}</p>
+          <p class="opacity-90 leading-none  font-medium  border-b mb-4 pb-4">{{t('contacts_card_tr_new_text_1')}}</p>
+          <p class="opacity-60 text-sm whitespace-pre-wrap mb-3">{{t('contacts_new_title2')}}</p>
+          <p class="opacity-90 leading-none  font-medium ">{{t('contacts_card_tr_new_text_2')}}</p>
         </div>
         <div class="col-span-2 rounded-xl">
           <img class="rounded-xl" src="~assets/images/tr.png" alt="">
