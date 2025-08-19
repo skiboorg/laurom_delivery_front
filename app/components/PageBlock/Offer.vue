b5f8adad90ff71b54c1fe13b068ba4414d6df465
xxx <hgfh@dfds.ddd>
2025-08-19 14:46:27 +0300

    fix

--- app/components/PageBlock/Offer.vue
+++ app/components/PageBlock/Offer.vue
@@ -5,12 +5,15 @@
 <template>
 <BlockSection class="!mb-5">
   <div class="container mt-10 md:!mt-20">
-    <TypingHeader50 class="text-center mb-7 " :text="$t('offer_text_0')"/>
+    <div class="mb-10">
+      <TypingHeader50 class="text-center mb-7 " :text="$t('offer_text_0')"/>
 
-    <div class="max-w-[1000px] m-auto relative">
-      <TypingGrayText class="absolute text-center max-w-full md:max-w-[590px] md:left-[50%] md:translate-x-[-50%]">{{$t('offer_text_1')}}</TypingGrayText>
-      <img class="w-full pt-14 md:pt-5"  src="~assets/map.svg">
+
+      <TypingGrayText class="text-center m-auto max-w-[590px] mb-8">{{$t('offer_text_1')}}</TypingGrayText>
+      <img class="w-full"  src="~assets/main.png">
     </div>
+
+
   </div>
 </BlockSection>
 </template>
